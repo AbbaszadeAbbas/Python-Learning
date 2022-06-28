63c42c772a2bc56980794d87a7009778527bcae5
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-06-28 10:27:28 +0400

    list( find the biggest number in a list )

---
+++ lists.py
@@ -0,0 +1,8 @@
+numbers = [1,3,5,7,9]
+max = numbers[0]
+
+for number in numbers:
+  if number > max:
+    max = number
+
+print(max)
---
+++ nested_loops.py
@@ -0,0 +1,6 @@
+number = [5,2,5,2,2]
+
+i = 0
+while i < len(number):
+  print(number[i] * 'x')
+  i+=1
